4c5556782539cd34c94da252dc03d7cd9f79d4ec
campitos <rapidclimate@gmail.com>
2015-07-04 06:01:07 -0500

    Clase martes 8 de junio

---
+++ src/main/java/unitec/maven/boot/Aplicacion.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.maven.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author campitos
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    @RequestMapping("/")
+    @CrossOrigin
+    @ResponseBody String hola(){
+        
+        return "Hola mundo";
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SpringApplication.run(Aplicacion.class, args);
+    }
+    
+}
