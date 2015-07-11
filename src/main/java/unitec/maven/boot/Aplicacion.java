1b4dfd279eee47f63d60cbfbdeafb54a524963ef
datoncito <rapidclimate@gmail.com>
2015-07-11 08:33:15 -0500

    Miau

--- src/main/java/unitec/maven/boot/Aplicacion.java
+++ src/main/java/unitec/maven/boot/Aplicacion.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +27,7 @@ public class Aplicacion {
 
   
     @RequestMapping("/")
+    @CrossOrigin
     @ResponseBody String hola(){
         
         return "Hola mundo";
